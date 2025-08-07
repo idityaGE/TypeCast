36ef1b0665b43cb7d60ea4034867bd35406440a4
idityaGE <am44910606@gmail.com>
2025-08-08 00:46:06 +0530

    good

--- src-tauri/src/setup/mod.rs
+++ src-tauri/src/setup/mod.rs
@@ -6,11 +6,7 @@ use crate::setup::tray::setup_tray;
 pub fn get_setup_handler() -> impl Fn(&mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
     |app| {
         let window = app.get_webview_window("main").unwrap();
-        window.set_always_on_top(true)?;
         window.set_ignore_cursor_events(true)?;
-        window.set_fullscreen(true)?;
-        window.set_skip_taskbar(true)?;
-        window.set_visible_on_all_workspaces(true)?;
 
         setup_tray(app)?;
         Ok(())
